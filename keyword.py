3caf79a4074c34df744fa972bb26b139dfc56c10
swadha-mrinal <swadha.mrinal@gmail.com>
2026-01-24 12:09:40 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,10 @@
+#printing a statment
+print("welcome to the world of programming")
+
+#printing a number
+print(6)
+
+#next line after printing
+print("hello python \n")
+import keyword
+print(keyword.kwlist)
