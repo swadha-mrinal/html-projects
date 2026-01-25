db6b65af0fab23a969155c39aabd897a1059c879
swadha-mrinal <swadha.mrinal@gmail.com>
2026-01-25 12:04:15 +0530

    Add files via upload

---
+++ pythonop.py
@@ -0,0 +1,20 @@
+num1=45
+num2=6
+print("number1",num1)
+print("number2",num2)
+print("Addition:", num1+num2)
+print("Difference:", num1-num2)
+print("product:", num1*num2)
+print("Division:", num1/num2)
+print("Floor division:", num1//num2)
+print("Modulus operation:", num1%num2)
+print("square:", num2**2)
+print("square root:", num1**0.5)
+
+print("Equal?", num1==num2)
+print("number1 greater?", num1>num2)
+print("number2 greater?", num1<num2)
+print("not Equal?", num1!=num2)
+
+result= num1/num2**2+10
+print("result of the given equasion is:",result )
