c9c73451e94f589bef4efc8df2b7170efc9b6c3c
swadha-mrinal <swadha.mrinal@gmail.com>
2026-01-25 11:47:18 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,22 @@
+Name="penguin"
+age= 15
+is_student=True
+weight=38.5
+
+
+print("Name:", Name)
+print("data type of name is:", type(Name))
+print("age:", age)
+print("data type of age is:", type(age))
+print("is_student:", is_student)
+print("data type of is_student is:",type(is_student) )
+print("weight:",weight )
+print("data type of weight is:", type(weight))
+
+print("\n after type casting....")
+age = str(age)
+print(age)
+print("data type of age is", type(age))
+weight=int(weight)
+print(weight)
+print("data type of weight is", type(weight))
