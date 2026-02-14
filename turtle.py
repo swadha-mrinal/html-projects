ce598df3990814d5f2bab295764e1939fcca2724
swadha-mrinal <swadha.mrinal@gmail.com>
2026-02-14 12:11:02 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("welcome to turtle window")
+
+board = turtle.Turtle()
+
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i = i+1
