902ddf0ca25afefecef516462404e098a3fedbce
swadha-mrinal <swadha.mrinal@gmail.com>
2026-02-22 11:58:45 +0530

    Add files via upload

---
+++ parrot2.py
@@ -0,0 +1,12 @@
+class parrot:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def sing(self,song):
+        return "{} sings {}".format(self.name,song)
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+    
+blu = parrot("Blu", 10)
+print(blu.sing("'happy'"))
+print(blu.dance())
